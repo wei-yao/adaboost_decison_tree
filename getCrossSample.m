6e53b6bd4f95d1504ee47fc5de93a7925b67d23b
weiyao <1033789547@qq.com>
2015-02-02 21:43:57 +0800

    随机置乱了原数据存入.mat文件

--- getCrossSample.m
+++ getCrossSample.m
@@ -1,7 +1,9 @@
 function [ trainData,trainLabel,testData,testLabel ] = getCrossSample( input,iterations,total )
 %用交叉验证的方法抽样
 %  输入：input 输入文件名  iterations 当前的轮数 范围是1到total ，total 总轮数.
-[measures,labels]=getData(input);
+% [measures,labels]=getData(input);
+%假设原数据在mat文件中，measures是属性，labels是类标， .mat 文件中的数据相比原数据做了列的混淆置乱.
+load(input);
 dataSize=size(measures,1);
 %每次抽取的样本大小.
 sampleSize=floor(dataSize/total);
@@ -28,14 +30,28 @@ function [measures,labels]=getData(input)
 %输入：含有数据的文件名
 %输出  measures :属性   labels： 标号
 
-%以下为读取鸢尾花数据的代码.
-[attrib1, attrib2, attrib3, attrib4, class] = textread(input, '%f%f%f%f%s', 'delimiter', ',');
-measures = [attrib1, attrib2, attrib3, attrib4];
-%数组总个数
-totalSize=size(measures,1);
-labels = zeros(totalSize, 1);
-labels(strcmp(class, 'Iris-setosa')) = 1;
-labels(strcmp(class, 'Iris-versicolor')) = 2;
-labels(strcmp(class, 'Iris-virginica')) = 3;
+% %以下为读取鸢尾花数据的代码.
+% [attrib1, attrib2, attrib3, attrib4, class] = textread(input, '%f%f%f%f%s', 'delimiter', ',');
+% measures = [attrib1, attrib2, attrib3, attrib4];
+% %数组总个数
+% totalSize=size(measures,1);
+% labels = zeros(totalSize, 1);
+% labels(strcmp(class, 'Iris-setosa')) = 1;
+% labels(strcmp(class, 'Iris-versicolor')) = 2;
+% labels(strcmp(class, 'Iris-virginica')) = 3;
+
+% 读取glass 数据.
+% [attrib1, attrib2, attrib3, attrib4,attrib5,attrib6,attrib7,attrib8,attrib9,attrib10, labels] = textread(input, '%f%f%f%f%f%f%f%f%f%f%f', 'delimiter', ',');
+% measures = [attrib1, attrib2, attrib3, attrib4,attrib5,attrib6,attrib7,attrib8,attrib9,attrib10];
+
+%读取 tic tac toe 数据.
+% [attrib1, attrib2, attrib3, attrib4,attrib5,attrib6,attrib7,attrib8,attrib9, labels] = textread(input, '%f%f%f%f%f%f%f%f%f%f', 'delimiter', ',');
+%  measures = [attrib1, attrib2, attrib3, attrib4,attrib5,attrib6,attrib7,attrib8,attrib9];
+% measureSize=size(measures,1);
+% %随机置乱原数据，当原数据按类排列时对结果影响比较大.
+%  permute=randsample(measureSize,measureSize);
+%  measures=measures(permute,:);
+%  labels=labels(permute);
+%  save('tic-tac-toe','measures','labels');
 end
 
