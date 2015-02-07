910eb48cf31649ac679b80211529bdbadb93c08a
weiyao <1033789547@qq.com>
2015-02-07 22:26:30 +0800

    添加了结果报告文档

--- customDecisionTree.m
+++ customDecisionTree.m
@@ -3,7 +3,7 @@ function [ accuracy ] = customDecisionTree( inputData )
 % 返回值：accuracy 模型在测试集数据上的准确度.
 %使用决策树训练模型并评估.
 accuracy=0;
-round=100;
+round=10;
 %使用k折交叉验证方法测试.
 for i=1:round
     %[trainData,trainLabel,testData,testLabel]=randomSelectData(inputData,850);
